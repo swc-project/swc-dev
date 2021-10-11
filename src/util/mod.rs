19b3625f4e5a621ce58d5fcfed856bc9a31ff41a
Donny <kdy1997.dev@gmail.com>
2021-10-11 20:12:34 +0900

    lint

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,14 +1,12 @@
 use anyhow::{anyhow, Error};
-use std::{
-    collections::{HashMap, HashSet},
-    fmt::Display,
-};
+use std::{collections::HashMap, fmt::Display};
 
 pub mod cargo;
 pub mod node;
 
 pub type AHashMap<K, V> = HashMap<K, V, ahash::RandomState>;
-pub type AHashSet<V> = HashSet<V, ahash::RandomState>;
+
+// pub type AHashSet<V> = HashSet<V, ahash::RandomState>;
 
 pub(crate) trait CargoEditResultExt<T>: Into<cargo_edit::Result<T>> {
     fn map_err_op(self, op: impl Display) -> Result<T, Error> {
