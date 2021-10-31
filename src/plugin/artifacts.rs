1391eef0dace4994a8924996481efddbeab8c3b5
Donny <kdy1997.dev@gmail.com>
2021-10-31 23:19:44 +0900

    Patch package.json

--- src/plugin/artifacts.rs
+++ src/plugin/artifacts.rs
@@ -1,17 +1,140 @@
-use crate::util::cargo::get_all_crates;
-use anyhow::Error;
+use crate::util::{cargo::get_all_crates, AHashMap};
+use anyhow::{bail, Context, Result};
+use serde_json::Value;
+use std::{
+    env::current_dir,
+    fs::{self, read_dir, read_to_string},
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 use structopt::StructOpt;
+use tracing::info;
 
-/// Patch package.json to include `optionalDependencies`
+/// Publish package for each platforms and patch package.json to include
+/// `optionalDependencies`
 #[derive(Debug, StructOpt)]
-pub struct ArtifactsCommand {}
+pub struct ArtifactsCommand {
+    /// Defaults whole crates.
+    #[structopt(long)]
+    pub crates: Vec<String>,
+
+    /// Defaults to current working directory.
+    #[structopt(long, default_value)]
+    pub artifacts_dir: String,
+}
 
 impl ArtifactsCommand {
-    pub fn run(&self) -> Result<(), Error> {
-        let crates = get_all_crates()?;
+    pub fn run(self) -> Result<()> {
+        let artifacts_dir = if self.artifacts_dir.is_empty() {
+            current_dir().context("failed to get current directory")?
+        } else {
+            PathBuf::from(self.artifacts_dir)
+        };
 
-        for (crate_name, manifest_dir) in crates {}
+        let enabled_crates = self.crates;
+
+        let crates = get_all_crates()?
+            .into_iter()
+            .filter(|(name, _)| enabled_crates.is_empty() || enabled_crates.contains(&name))
+            .collect::<AHashMap<_, _>>();
+
+        let all_pkg_platforms =
+            get_swc_pkg_files(&artifacts_dir).context("failed to get swc package files")?;
+
+        info!("Using {:?}", all_pkg_platforms);
+
+        for (crate_name, manifest_dir) in crates {
+            let base_package_json_path = manifest_dir.join("package.json");
+            assert!(
+                base_package_json_path.is_file(),
+                "package.json for crate `{}` is not found",
+                crate_name
+            );
+
+            let base_package_json_str = read_to_string(&base_package_json_path).context(
+                format!("failed to read `{}`", base_package_json_path.display()),
+            )?;
+
+            let mut base_package_json = Value::from_str(&base_package_json_str)?;
+            if !base_package_json.is_object() {
+                bail!("package.json is not an object")
+            }
+            {
+                let pkg_json_obj = base_package_json.as_object_mut().unwrap();
+
+                if !pkg_json_obj.contains_key("optionalDependencies") {
+                    pkg_json_obj.insert(
+                        "optionalDependencies".to_string(),
+                        Value::Object(Default::default()),
+                    );
+                }
+
+                let pkg_name = pkg_json_obj
+                    .get("name")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string();
+                let pkg_version = pkg_json_obj
+                    .get("version")
+                    .unwrap()
+                    .as_str()
+                    .unwrap()
+                    .to_string();
+
+                let opt_deps = pkg_json_obj
+                    .get_mut("optionalDependencies")
+                    .unwrap()
+                    .as_object_mut()
+                    .unwrap();
+
+                let pkg_platforms = all_pkg_platforms.get(&crate_name).with_context(|| {
+                    format!("failed to get package files for crate `{}`", crate_name)
+                })?;
+
+                for platform in pkg_platforms {
+                    let dep_name = format!("{}-{}", pkg_name, platform);
+                    if !opt_deps.contains_key(&dep_name) {
+                        opt_deps.insert(dep_name, Value::String(pkg_version.clone()));
+                    }
+                }
+            }
+            let pkg_json_str = serde_json::to_string_pretty(&base_package_json)?;
+
+            fs::write(&base_package_json_path, &pkg_json_str).context(format!(
+                "failed to write to `{}`",
+                base_package_json_path.display()
+            ))?;
+        }
 
         todo!()
     }
+}
+
+/// Key is crate name and values are platforms
+fn get_swc_pkg_files(artifacts_dir: &Path) -> Result<AHashMap<String, Vec<String>>> {
+    let entries = read_dir(&artifacts_dir)?;
+
+    let mut buf = AHashMap::<_, Vec<_>>::default();
+
+    for e in entries {
+        let e = e?;
+        let path = e.path();
+
+        if path.to_string_lossy().ends_with(".swc-pkg.tgz") {
+            let file_name = path.file_name().unwrap().to_string_lossy();
+
+            assert_eq!(
+                file_name.split('.').count(),
+                4,
+                "The plugin artifact should be named `<crate_name>.<platform_name>.swc-pkg.tgz`"
+            );
+            let crate_name = file_name.split('.').next().unwrap().to_string();
+            let platform = file_name.split('.').nth(1).unwrap().to_string();
+
+            buf.entry(crate_name).or_default().push(platform);
+        }
+    }
+
+    Ok(buf)
 }
